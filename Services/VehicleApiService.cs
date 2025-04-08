2ea294339a0f16c93bf2de9f29f77ba5487f7dbd
NiklasRoos91 <niklas_roos@live.se>
2025-04-08 23:32:07 +0200

    Implemented external API integration for vehicle makes validation. The API is used to verify that the make provided by the user is valid. Also added extension methods for cleaner service configuration.

---
+++ Services/VehicleApiService.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using WorkshopBookingSystemWebAPI.Interfaces;
+using System.Threading;
+
+namespace WorkshopBookingSystemWebAPI.Services
+{
+    public class VehicleApiService : IVehicleApiService
+    {
+        private readonly HttpClient _httpClient;
+        private List<string> _cachedMakes;
+        private bool _dataLoaded = false;
+
+        public VehicleApiService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<string>> GetVehicleMakesAsync(CancellationToken cancellationToken)
+        {
+            if (!_dataLoaded)
+            {
+                await LoadVehicleMakesAsync(cancellationToken);
+            }
+
+            return _cachedMakes;
+        }
+
+        public async Task LoadVehicleMakesAsync(CancellationToken cancellationToken)
+        {
+            var response = await _httpClient.GetStringAsync("https://vpic.nhtsa.dot.gov/api/vehicles/getallmakes?format=json", cancellationToken);
+            var jsonResponse = JsonConvert.DeserializeObject<JObject>(response);
+
+            _cachedMakes = jsonResponse["Results"]
+            .Select(m => m["Make_Name"].ToString())
+            .ToList();
+
+            _dataLoaded = true;
+        }
+
+        public async Task<bool> DoesVehicleMakeExistAsync(string vehicleMake, CancellationToken cancellationToken)
+        {
+            if (!_dataLoaded)
+            {
+                await LoadVehicleMakesAsync(cancellationToken);
+            }
+
+            return _cachedMakes.Contains(vehicleMake, StringComparer.OrdinalIgnoreCase);
+        }
+    }
+}
